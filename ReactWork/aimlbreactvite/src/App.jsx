d46fd28992b1e7a1863b42738608413e810c4890
ArpitSeth-232 <arpitseth232@gmail.com>
2024-11-19 10:41:59 +0530

    props

--- ReactWork/aimlbreactvite/src/App.jsx
+++ ReactWork/aimlbreactvite/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './App.css'
+import Student from './Student.jsx'
+import logo from './assets/OIP (1).jpeg'
 
 function App() {
 
@@ -13,10 +15,34 @@ function App() {
     
 
   }
+
+  const student_data = {
+    name : "seth",
+    roll : 45,
+    college: "ABES Engineering College",
+    sec : "B",
+    branch : "Aiml",
+    pic : 'https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/car_4.png'
+    
+  }
   return (
-    <div style={{color:'red'}} className='Name'> 
-    <h2>Abes Engineering College</h2>
-    <div style={mystyle}>{a}</div>
+
+   
+
+    <div style={{color:'black'}}> 
+    {/* <h2>App</h2> */}
+
+
+    {/* <Student college="ABES Engineering College" sec="B" roll="45" name = "Seth" branch ="Aiml" pic = {<img src={logo} height={100} width={100}/>}/>
+    <Student  sec="B" roll="46" name = "Tomar" branch ="Aiml" pic = {<img src={logo} height={100} width={100}/>}/>
+    <Student college="ABES Engineering College" sec="B" roll="47" name = "Diwedi" branch ="Aiml" pic = {<img src={logo} height={100} width={100}/>}/> */}
+
+    <Student data = {student_data}/>
+
+
+
+
+
     </div>
   )
 }
