2ae93e325349787db504ac0e319674924091fe5d
ArpitSeth-232 <arpitseth232@gmail.com>
2024-12-03 10:28:33 +0530

    useState and useEffect

--- ReactWork/aimlbreactvite/src/Image_manipulation.jsx
+++ ReactWork/aimlbreactvite/src/Image_manipulation.jsx
@@ -2,15 +2,7 @@ import React,{useState} from 'react'
 import './Image_manipulation.css'
 import logo from './assets/OIP (1).jpeg'
 
-const [bg,setbg] = useState("black");
-const [ht,setht] = useState(500);
-const [wd,setwd] = useState(100);
-const [rt,setrt] = useState(0);
 
-
-const change = () =>{
-    
-}
 
 
 
@@ -18,17 +10,55 @@ const change = () =>{
 
 
 function Image_manipulation() {
+
+  const [bg,setbg] = useState("black");
+  const [ht,setht] = useState(200);
+  const [wd,setwd] = useState(300);
+  const [rt,setrt] = useState(0);
+
+  const [red,setred] = useState(0);
+  const [blue,setblue] = useState(0);
+  const [green,setgreen] = useState(0);
+
+
+  
+  
+  const change = () =>{
+    
+      setred(Math.random() * 255);
+      setblue(Math.random() * 255);
+      setgreen(Math.random() * 255);
+
+      setbg(`rgb(${red},${green},${blue})`)
+      
+  }
+
+  const heightEnhance = ()=>{
+    setht(ht+20);
+  }
+
+  const widthEnhance = ()=>{
+    setwd(wd+20);
+  }
+
+  const rotate = ()=>{
+    
+    setrt(rt+30);
+    
+  }
+
+  
+
   return (
     <div className='imageCard'>
-
-        <div className='imgdiv'><img src={logo} alt="" /></div>
+        <div className='imgdiv' style={{backgroundColor:bg}}><img src={logo} alt="" height={ht} width={wd} style={{transform:`rotate(${rt}deg)`}}/></div>
 
         <div className='btn'>
 
             <button onClick={change}>Change Background</button>
-            <button>Change Height</button>
-            <button>Rotate</button>
-            <button>Change Width</button>
+            <button onClick={heightEnhance}>Change Height</button>
+            <button onClick={rotate}>Rotate</button>
+            <button onClick={widthEnhance}>Change Width</button>
 
         </div>
 
